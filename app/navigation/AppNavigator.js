3b5201c30d9d286a6bdbfec3f7acb030c643f748
Ida Johansson <ida.johansson@consid.se>
2024-07-27 18:37:30 +0200

    changes icon

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,35 +1,18 @@
 import * as React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+
 const Tab = createBottomTabNavigator();
-
-
-function HomeScreen() {
-   return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Text>Home Screen</Text>
-     </View>
-   );
- }
-
- function DetailsScreen() {
-   return (
-     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <Text>Details Screen</Text>
-     </View>
-   );
- }
-
 
 
 export default function AppNavigator() {
    return (
       <Tab.Navigator>
-         <Tab.Screen name="Home" component={HomeScreen} />
-         <Tab.Screen name="Details" component={DetailsScreen} />
+         <Tab.Screen name="Hem" component={HomeScreen} />
+         <Tab.Screen name="Övrigt" component={DetailsScreen} />
      </Tab.Navigator>
      );
 }
